304db0ed70bbcff0efa86de8bc70494eee38949d
AlexanderLuasan <38295434+AlexanderLuasan@users.noreply.github.com>
2018-09-18 14:11:31 +0800

    add packages and init in contol

--- Acykin/control.cpp
+++ Acykin/control.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
 #include "control.h"
-
+#include <allegro5/allegro.h>
 using namespace std;
 
 
 int controlStart() {
 
 	cout << "where the main game loop is" << endl;
+	al_init();
+
+	ALLEGRO_TIMER *timer;
+	timer = al_create_timer(1.0 / 60.0);
+	al_start_timer(timer);
+
+
+	ALLEGRO_EVENT_QUEUE *queue;
+	queue = al_create_event_queue();
+
+
+
+
 
 	return 0;
 
