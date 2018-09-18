1ec432185b1823ec76446a9c17f3aa948f29851e
AlexanderLuasan <38295434+AlexanderLuasan@users.noreply.github.com>
2018-09-18 14:06:23 +0800

    add the files

---
+++ Acykin/control.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "control.h"
+
+using namespace std;
+
+
+int controlStart() {
+
+	cout << "where the main game loop is" << endl;
+
+	return 0;
+
+}
--- Acykin/main.cpp
+++ Acykin/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include "control.h"
+
 
 using namespace std;
 
 int main() {
 	cout << "Acykin a game" << endl;
+
+	controlStart();
 
 	system("pause");
 	return 0;
