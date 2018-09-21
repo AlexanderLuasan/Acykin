6b616d8bde5d01a243829c3325e1d16169de63a3
AlexanderLuasan <38295434+AlexanderLuasan@users.noreply.github.com>
2018-09-21 13:56:53 +0800

    comments

--- Acykin/xmlparser.h
+++ Acykin/xmlparser.h
@@ -5,30 +5,30 @@
 using namespace std;
 
 
-class xmltag
+class xmltag//a tag of html
 {
 public:
 	xmltag();
-	xmltag(vector<string> data);
-	string getname();
-	string getleftover();
-	xmltag get(string name);
-	string head(string name);
-	void options();
-	int getendindex();
-	vector<string> getkeys();
-	vector<string> getckeys();
-	void nameappend(string add);
+	xmltag(vector<string> data); //do not use recurvie formula
+	string getname();//get name same as head("name")
+	string getleftover(); // any non processdata data
+	xmltag get(string name);//move down a tag
+	string head(string name); //read data out of a head line
+	void options(); //couts things to read
+	int getendindex(); // used in recursive function to tells you the number of lines in the tag
+	vector<string> getkeys(); //gets keys for other tags
+	vector<string> getckeys(); // gets keys for header data
+	void nameappend(string add); //used to change the name of the tag
 
 private:
-	vector<string> keys;
-	vector<xmltag> values;
-	map<string, string> content;
-	vector<string> ckeys;
-	string leadingline;
-	string tagtype;
-	string raw;
-	int endindex;
+	vector<string> keys;//list of lower tags
+	vector<xmltag> values; // path to lower tags
+	map<string, string> content; //header data map
+	vector<string> ckeys; // list of header data keys
+	string leadingline; // used in passing likely grabage
+	string tagtype; // what type of tag
+	string raw; // leftover data not tag opens or closes
+	int endindex; // using in recursive
 };
 
 
